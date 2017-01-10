707051964fb3385213e3044d4d1ac0e585b7f6bb
Jason <jason@nosaj.io>
2017-01-10 19:48:30 +0000

    Animate in Front elements

--- app/views/Front/index.jsx
+++ app/views/Front/index.jsx
@@ -18,7 +18,8 @@ const Front = React.createClass({
     return (
       <main className="front-view">
         <header className="front-view__introduction">
-          <h1><strong>I'm a multidisciplinary designer and software engineer.</strong> I've built stuff for <a href="https://williamhill.com" target="_blank">William Hill Online</a>, <a href="http://mrsite.com" target="_blank">MrSite</a>, <a href="http://beachfix.co" target="_blank">BeachFix</a>, and the BBC (via <a href="https://metabroadcast.com" target="_blank">Meta</a>).</h1>
+          <h1>I'm a multidisciplinary designer and software engineer.</h1>
+          <p className="subhead">I've built stuff for <a href="https://williamhill.com" target="_blank">William Hill Online</a>, <a href="http://mrsite.com" target="_blank">MrSite</a>, <a href="http://beachfix.co" target="_blank">BeachFix</a>, the BBC (via <a href="https://metabroadcast.com" target="_blank">Meta</a>) and more.</p>
         </header>
         <ul className="front-view__actions">
           <li
@@ -32,7 +33,7 @@ const Front = React.createClass({
           <Posts posts={allPosts}/>
         </section>
         <footer className="front-view__footer">
-          
+
         </footer>
       </main>
     )
