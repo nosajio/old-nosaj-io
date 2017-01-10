c724956a20e28704e91b7804b81e63a8541f1a1e
Jason <jason@nosaj.io>
2017-01-10 20:56:00 +0000

    Add google analytics

--- app/views/App/index.jsx
+++ app/views/App/index.jsx
@@ -14,8 +14,17 @@ const App = React.createClass({
     this.setState({ freshRender: true });
   },
 
+  componentDidMount () {
+    this.putGaOnPage();
+    this.sendEventToGa();
+  },
+
   componentWillReceiveProps (nextProps) {
     this.setState({ freshRender: false });
+  },
+
+  componentDidUpdate () {
+    this.sendEventToGa();
   },
 
   /**
@@ -37,6 +46,19 @@ const App = React.createClass({
 
   handleStorePosts(json) {
     this.setState({ allPosts: json });
+  },
+
+  sendEventToGa () {
+    ga('send', 'pageview');
+  },
+
+  putGaOnPage () {
+    // Google analytics code from google
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-71692329-1', 'auto');
   },
 
   render () {
