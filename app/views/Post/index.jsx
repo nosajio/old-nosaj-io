4e381a1e7709c474b9bfbd5f94167d84c2164520
Jason <jason@nosaj.io>
2017-01-10 00:57:10 +0000

    Style bulk of post view

--- app/views/Post/index.jsx
+++ app/views/Post/index.jsx
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react'
 
+import './post.scss';
+
 const Post = (props, context) => {
   const slug = props.params.slug;
   const posts = props.data.allPosts;
@@ -12,11 +14,18 @@ const Post = (props, context) => {
   const post = posts.filter(post => post.slug === slug)[0];
 
   return (
-    <main className="post-view">
-      <article className="post-view__body">
+    <main
+      className={`post-view ${post.slug}`}>
+      <link rel="stylesheet" href={`/api/posts/${post.slug}/css`}></link>
+      <article className="post-view__container">
         <header className="post-view__header">
-          <h1>{post.title}</h1>
+          <h1 className="post-title">{post.title}</h1>
+          <ul className="post-meta">
+            <li className="post-meta__item">{post.friendlyDate}</li>
+            <li className="post-meta__item">{post.readingTime} min read</li>
+          </ul>
         </header>
+        <div className="post-view__body" dangerouslySetInnerHTML={{__html: post.body}}></div>
       </article>
     </main>
   )
