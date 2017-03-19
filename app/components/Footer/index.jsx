9edcf68ffd124c095972b853db519a97b82a2d25
Jason Howmans <jason@nosaj.io>
2017-03-19 16:24:06 +0000

    Change the email format in the footer

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -12,7 +12,7 @@ const Footer = () => (
         <a href="https://twitter.com/jhwmns" target="_blank">Twitter</a>
       </li>
       <li className="footer-list__item">
-        <a href="mailto:jason+www@nosaj.io" target="_blank">Email</a>
+        <a href="mailto:Jason Howmans<jason+www@nosaj.io>" target="_blank">Email</a>
       </li>
     </ul>
   </footer>
