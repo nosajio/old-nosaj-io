72a4d9ea02f5970fdff7e4b4323cd605f2f5baa9
Jason Howmans <jason@nosaj.io>
2017-03-19 19:58:15 +0000

    Make the augmenter able to handle async
    - An augment method can now return a promise and it'll work

--- server/lib/augment.js
+++ server/lib/augment.js
@@ -3,21 +3,39 @@ const path = require('path');
 module.exports = augment;
 
 function augment(augmenters, writing) {
-  'use strict';
   if (! augmenters) {
     return console.error('Augmenters array required for augment to work');
   }
 
   const augmentersDir = path.join('..', 'api', 'augmenters');
-  const augmentedwriting = writing.map(augmentNote);
+  const augmentedPosts = writing.map(augmentPost);
 
-  return augmentedwriting;
+  return Promise.all(augmentedPosts);
 
-  function augmentNote(note) {
-    let clonedNote = Object.assign({}, note);
-    augmenters.forEach((aug) => {
-      clonedNote = require( path.join(augmentersDir, aug) )(clonedNote);
+  function augmentPost(post) {
+    const asyncOutputs = [];
+    let clonedPost = Object.assign({}, post);
+    augmenters.forEach(augFile => {
+      let augmentedOutput = require( path.join(augmentersDir, augFile) )(clonedPost);
+      if ('then' in augmentedOutput) {
+        asyncOutputs.push(augmentedOutput);
+        return;
+      }
+      clonedPost = augmentedOutput;
+      return clonedPost;
     });
-    return clonedNote;
+    // In order to properly manage both pure augment functions and async ones, 
+    // wrap every response in a promise so that they can be handled by Promise.all
+    // and passed back to the then.then.then... stream as the expected array of posts.
+    if (asyncOutputs.length) {
+      return new Promise(resolve => {
+        Promise.all(asyncOutputs).then(asyncAugs => {
+          asyncAugs.forEach(a => Object.assign(clonedPost, a));
+          resolve(clonedPost);
+        });
+      });
+    } else {
+      return Promise.resolve(clonedPost);
+    }
   }
 }
