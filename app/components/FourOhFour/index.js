818504b226e18440e0a36d832318afe6d0f82e68
Jason Howmans <jason@nosaj.io>
2017-03-27 00:32:57 +0100

    Improve 404 messaging

--- app/components/FourOhFour/index.js
+++ app/components/FourOhFour/index.js
@@ -1,20 +1,27 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+import Logo from '../Logo';
+import Footer from '../Footer';
 
 import './four-oh-four.scss'
 
-const FourOhFour = ({ actions }) => {
+const FourOhFour = ({ actions, data: { navigateTo } }) => {
   // Send this as a custom event to GA for analysis
   actions.trackEvent('Errors', '404');
   return (
     <section className="four-oh-four">
-      <h1>You Broke It</h1>
-      <p>Not really but whatever you're looking for, it ain't here.</p>
+      <Logo onNavigate={navigateTo} text="nosaj" />
+      <h1 className="four-oh-four__title">DERP <sup>404</sup></h1>
+      <p>The thing you're looking for seems to have gone. While you're here, why not <Link to="/">read my posts</Link> or <Link to="/portfolio">check out my portfolio</Link></p>
+      <Footer />
     </section>
   );
 }
 
 FourOhFour.propTypes = {
   actions: PropTypes.object,
+  data: PropTypes.object,
 }
 
 
