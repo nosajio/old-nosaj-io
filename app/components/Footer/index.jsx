e18a68ec403d947bc3bc63adfcb8dc53bea2933d
Jason Howmans <jason@nosaj.io>
2017-04-10 21:46:30 +0100

    Update social accounts since the big change of 2017

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -6,10 +6,10 @@ const Footer = () => (
   <footer className="the-footer">
     <ul className="footer-list">
       <li className="footer-list__item">
-        <a href="https://github.com/nosajj" target="_blank">GitHub</a>
+        <a href="https://github.com/nosajio" target="_blank">GitHub</a>
       </li>
       <li className="footer-list__item">
-        <a href="https://twitter.com/jhwmns" target="_blank">Twitter</a>
+        <a href="https://twitter.com/__nosaj" target="_blank">Twitter</a>
       </li>
       <li className="footer-list__item">
         <a href="mailto:Jason Howmans<jason+www@nosaj.io>" target="_blank">Email</a>
