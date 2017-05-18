fdadbe86552ce12b652ed4e2f3bbc61f81d0bada
Jason Howmans <jason@nosaj.io>
2017-05-18 13:21:42 +0100

    Front page changes
    - Availability text
    - Intro message

--- app/components/FrontIntro/index.jsx
+++ app/components/FrontIntro/index.jsx
@@ -4,8 +4,8 @@ import './frontIntro.scss';
 
 const FrontIntro = ({ className, toggleMessageUi }) => (
   <section className={`front-intro ${className}`}>
-    <p className="front-intro__text">Hi I'm Jason. I'm a multi-disciplinary designer and software engineer, and I currently work with small teams to build ambitious products. I've worked on projects for The BBC, MetaBroadcast, William Hill Online, and many others.</p>
-    <p className="front-intro__subtext"><Button onClick={() => toggleMessageUi(true)}>Get in touch</Button> <span className="front-intro__booking">I'm fully booked until the end of April.</span></p>
+    <p className="front-intro__text">Hi I'm Jason. I'm a multi-disciplinary designer and software engineer. I currently work with small teams to build ambitious products. I've worked on stuff for The BBC, MetaBroadcast, William Hill Online and many others.</p>
+    <p className="front-intro__subtext"><Button onClick={() => toggleMessageUi(true)}>Get in touch</Button> <span className="front-intro__booking">I'm accepting new projects.</span></p>
   </section>
 );
 
